2a5b9f51b70672ef77109d892062267b2d7be5a1
patel-malav <patelmalav64@gmail.com>
2020-07-10 00:17:43 +0530

    📨 Chatbox UI

--- src/app/chat/chat-box/chat-box.component.ts
+++ src/app/chat/chat-box/chat-box.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { MessagesService } from '../messages.service';
+
 @Component({
   selector: 'app-chat-box',
   templateUrl: './chat-box.component.html',
-  styleUrls: ['./chat-box.component.less']
+  styleUrls: ['./chat-box.component.less'],
 })
 export class ChatBoxComponent implements OnInit {
+  constructor(private messages: MessagesService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
